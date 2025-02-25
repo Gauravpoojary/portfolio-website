4a231dbcca78885ddcf463c981208b266f83877e
GauravPoojary <gauravpoojary312@gmail.com>
2025-02-25 23:39:22 +0530

    About Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Portfolio Website Loaded");
+
+    // Navbar Animation
+    window.addEventListener("scroll", function() {
+        let navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {
+            navbar.classList.add("shadow");
+        } else {
+            navbar.classList.remove("shadow");
+        }
+    });
 });
